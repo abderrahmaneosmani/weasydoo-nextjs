abc8ffa2e0635a49dc57c7ea1ea92b6f3e58ac90
Osmani Abderrahmane <abderrahmaneosmani2@gmail.com>
2024-05-16 17:29:22 +0100

    handle login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,61 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
-import  CredentialsProvider from 'next-auth/providers/credentials'
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    pages:{
-        signIn:'/auth/login'
+  pages: {
+    signIn: "/auth/login",
+  },
+  callbacks: {
+    async signIn() {
+      return true;
     },
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        return true
-      },
-      async redirect({ url, baseUrl }) {
-        return baseUrl
-      },
-      async session({ session, user, token }) {
-        return session
-      },
-      async jwt({ token, user, account, profile, isNewUser }) {
-        return token
-      }},
-    
-    providers: [
-       CredentialsProvider({
-            name: "Credentials",
-         
-            credentials: {
-              email: { label: "root", type: "text", placeholder: "" },
-              password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials:any, req) {
-              const {email,password }=credentials
-            
-              const dataCredentials={
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session }) {
+      return session;
+    },
+  },
 
-                email,password
-              }
-              const myHeaders = new Headers();
-              myHeaders.append("Content-Type", "application/json");
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
 
-              const response = await fetch('https://fakestoreapi.com/auth/login', {
-                method: 'POST',
-                headers:myHeaders,
-                body: JSON.stringify(dataCredentials)
-              })
-          
-              const data = await response.json()
-        
-              if (data && response.ok) {
-                return data.user
-              } else {
-                return null
-        
-              }
-            }
-          })
-    ]
-    
-})
+      credentials: {
+        email: { label: "root", type: "text", placeholder: "" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any, req) {
+        try {
+          const { username, password } = credentials;
 
-export { handler as GET, handler as POST }
+          const dataCredentials: any = {
+            username,
+            password,
+          };
+          const myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/json");
+
+          const response = await fetch("https://fakestoreapi.com/auth/login", {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify(dataCredentials),
+          });
+
+          const data = await response.json();
+
+          if (data && response.ok) {
+            return data?.token;
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.log("error", error);
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,19 +1,51 @@
-import React from 'react'
+"use client";
+
+import { signIn } from "next-auth/react";
+import { FormEvent } from "react";
 
 function LoginPage() {
+  async function handleSubmitLogin(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formLogin = new FormData(event?.currentTarget);
+    const username = formLogin.get("username");
+    const password = formLogin.get("password");
+
+    signIn("credentials", {
+      username,
+      password,
+    }).then((res) => res);
+  }
+
   return (
-    <div className='flex items-center justify-center mt-20 flex-col'>
-        <p className='text text-3xl text-blue-700'>LoginPage</p>
+    <div className="flex items-center justify-center mt-20 flex-col">
+      <p className="text text-3xl text-blue-700">Login Page</p>
 
-         <div className='flex flex-col justify-start items-center mt-20 '>
-            <input type="text"  placeholder='email' className='border border-gray-300 rounded-md px-4 py-2  my-2 focus:outline-none focus:border-blue-500'/>
-            <input type="text" placeholder='password' className='border border-gray-300 round-md px-4 py-2  mb-2 focus:outline-none focus:border-blue-500' />
-            <button className= " mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Connect </button>
-
-
-         </div>
+      <div className="flex flex-col justify-start items-center mt-20 ">
+        <form onSubmit={handleSubmitLogin} className="flex flex-col">
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            className="border border-gray-300 rounded-md px-4 py-2  my-2 focus:outline-none focus:border-blue-500"
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            className="border border-gray-300 round-md px-4 py-2  mb-2 focus:outline-none focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            className=" mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            {" "}
+            Connect{" "}
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
